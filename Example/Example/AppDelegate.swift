c5b94f758e011e1d64c26ebe5ab7a0b3888ef179
wayne.hsu <>
2025-09-03 08:37:59 +0800

    [Feature] Add example code

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -32,7 +32,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    // 如果專案有 SceneDelegate 則不會觸發這段，所以需要寫在 SceneDelegate
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        CrowPush.openURL(url: url)
+        return true
+    }
+    
+    
+    
 }
 
